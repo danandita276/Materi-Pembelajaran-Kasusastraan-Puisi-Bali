11db294b2326d95ea8ac3e3e78f7f8f011fc213c
danandita276 <ditadanan53@gmail.com>
2025-02-06 14:42:43 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5WBIJNprMJ4":
+        Script1();
+        break;
+      case "5Zrab3LiVkY":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.pause();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+}
+
